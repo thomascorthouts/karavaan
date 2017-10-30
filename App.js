49ea8093830f77948b3b310493eca2534e602e3f
Mathias Spanhove <mathias.spanhove@student.ucll.be>
2017-10-30 13:29:21 +0100

    Configured React-Native application with typescript

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Login } from './build/components/Login';
+
+
+export default class App extends React.Component {
+  render() {
+    return <Login />;
+  }
+}
---
+++ src/components/Login.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import LoginForm from './build/components/LoginForm';
+
+export class Login extends React.Component {
+    render() {
+        return (<View style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require('../../images/Karavaan.png')} />
+            </View>
+            <View style={styles.formContainer}>
+                </LoginForm>
+            </View>
+        </View>);
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#4B9382'
+    },
+    logoContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexGrow: 1
+    },
+    logo: {
+        width: 200,
+        height: 200
+    },
+    title: {
+        color: '#FFF',
+        marginTop: 10,
+        width: 160
+    }
+});
