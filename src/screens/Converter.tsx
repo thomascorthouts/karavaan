3fe039b25d29a2ff400cdd810e4cf48152b50f91
thomascorthouts <thomas.corthouts@icloud.com>
2017-11-28 13:45:45 +0100

    temporary push

--- src/screens/Converter.tsx
+++ src/screens/Converter.tsx
@@ -1,41 +1,54 @@
 import React, {Component} from 'react';
 import {ScrollView, Text} from 'react-native';
 
-class Converter extends Component {
-    currencies: { [index: string]: number } = {
-        'AUD': 1.5503,
-        'BGN': 1.9558,
-        'BRL': 3.8288,
-        'CAD': 1.4969,
-        'CHF': 1.1609,
-        'CNY': 7.7755,
-        'CZK': 25.469,
-        'DKK': 7.442,
-        'EUR': 1,
-        'GBP': 0.8881,
-        'HKD': 9.1775,
-        'HRK': 7.577,
-        'HUF': 313.54,
-        'IDR': 15880.0,
-        'ILS': 4.1392,
-        'INR': 76.251,
-        'JPY': 131.61,
-        'KRW': 1280.2,
-        'MXN': 22.098,
-        'MYR': 4.8292,
-        'NOK': 9.6723,
-        'NZD': 1.7183,
-        'PHP': 59.499,
-        'PLN': 4.2155,
-        'RON': 4.6525,
-        'RUB': 68.948,
-        'SEK': 9.8818,
-        'SGD': 1.5885,
-        'THB': 38.466,
-        'TRY': 4.6557,
-        'USD': 1.1749,
-        'ZAR': 16.309
+interface IState {
+    currencies: ICurrencies;
+};
+
+interface ICurrencies{
+
+    'AUD': number;
+    'BGN': number;
+    'BRL': number;
+    'CAD': number;
+    'CHF': number;
+    'CNY': number;
+    'CZK': number;
+    'DKK': number;
+    'GBP': number;
+    'HKD': number;
+    'HRK': number;
+    'HUF': number;
+    'IDR': number;
+    'ILS': number;
+    'INR': number;
+    'JPY': number;
+    'KRW': number;
+    'MXN': number;
+    'MYR': number;
+    'NOK': number;
+    'NZD': number;
+    'PHP': number;
+    'PLN': number;
+    'RON': number;
+    'RUB': number;
+    'SEK': number;
+    'SGD': number;
+    'THB': number;
+    'TRY': number;
+    'USD': number;
+    'ZAR': number;
+}
+
+class Converter extends Component< {}, IState> {
+
+    state = {
+        currencies: {} as IState
     };
+
+    constructor(state: IState) {
+        super(state);
+    }
 
     render() {
         return (
@@ -46,7 +59,7 @@ class Converter extends Component {
     }
 
     convert = (amount: number, from: string, to: string) => {
-        return amount * this.getRate(this.currencies, from, to);
+        return amount * this.getRate(this.state.currencies, from, to);
 
     }
 
@@ -58,11 +71,10 @@ class Converter extends Component {
 
     }
 
-    fetchData = (currencies: { [index: string ]: number }, callback: Function) => {
+    fetchData = () => {
         fetch('https://api.fixer.io/latest')
             .then((resp) => resp.json())
-            .then((data) => currencies = data.rates)
-            .then(callback(currencies));
+            .then((data) => this.currencies = data.rates);
 
     }
 }
