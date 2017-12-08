c3660ba0cfe7539924edde0594ad1e9d501faee5
Mathias Spanhove <mathias.spanhove@student.ucll.be>
2017-12-08 23:05:26 +0100

    fixed function call

--- src/screens/ExpenseTab/AddExpense.tsx
+++ src/screens/ExpenseTab/AddExpense.tsx
@@ -30,8 +30,6 @@ export class AddExpense extends Component<IDefaultNavProps, IState> {
             expenseArrayId: this.props.navigation.state.params.expenseArrayId || 'expenses',
             error: ''
         };
-
-
     }
 
     render() {
@@ -51,8 +49,6 @@ export class AddExpense extends Component<IDefaultNavProps, IState> {
 
                 <Text style={styles.title}>New Expense</Text>
                 <Text style={styles.errorStyle}> {this.state.error} </Text>
-
-
 
                 <KeyboardAvoidingView behavior='padding'>
                     <InputWithoutLabel
@@ -90,7 +86,6 @@ export class AddExpense extends Component<IDefaultNavProps, IState> {
                         autoCapitalize={'words'}
                     />
 
-
                     <View style={{ flexDirection: 'row' }}>
                         <View style={styles.inputAmount}>
                             <InputWithoutLabel
@@ -112,42 +107,42 @@ export class AddExpense extends Component<IDefaultNavProps, IState> {
                                 const expense = Object.assign({}, this.state.expense, { currency: itemValue });
                                 this.setState({ expense });
                             }} >
-                            <Picker.Item label="EUR / €" value="EUR" />
-                            <Picker.Item label="USD / $" value="USD" />
-                            <Picker.Item label="AUD / $A" value="AUD" />
-                            <Picker.Item label="BGN / Lv" value="BGN" />
-                            <Picker.Item label="BRL / R$" value="BRL" />
-                            <Picker.Item label="CAD / C$" value="CAD" />
-                            <Picker.Item label="CHF / SFr" value="CHF" />
-                            <Picker.Item label="CNY / C$" value="CNY" />
-                            <Picker.Item label="CZK / C$" value="CZK" />
-                            <Picker.Item label="DKK / C$" value="DKK" />
-                            <Picker.Item label="GBP / £" value="GBP" />
-                            <Picker.Item label="HKD / £" value="HKD" />
-                            <Picker.Item label="HRK / £" value="HRK" />
-                            <Picker.Item label="HUF / £" value="HUF" />
-                            <Picker.Item label="IDR / £" value="IDR" />
-                            <Picker.Item label="ILS / £" value="ILS" />
-                            <Picker.Item label="INR / £" value="INR" />
-                            <Picker.Item label="JPY / £" value="JPY" />
-                            <Picker.Item label="KRW / £" value="KRW" />
-                            <Picker.Item label="MXN / £" value="MXN" />
-                            <Picker.Item label="MYR / £" value="MYR" />
-                            <Picker.Item label="NOK / £" value="NOKk" />
-                            <Picker.Item label="NZD / £" value="NZD" />
-                            <Picker.Item label="PHP / £" value="PHP" />
-                            <Picker.Item label="PLN / £" value="PLN" />
-                            <Picker.Item label="RON / £" value="RON" />
-                            <Picker.Item label="RUB / £" value="RUB" />
-                            <Picker.Item label="SEK / £" value="SEK" />
-                            <Picker.Item label="SGD / £" value="SGD" />
-                            <Picker.Item label="THB / £" value="THB" />
-                            <Picker.Item label="TRY / £" value="TRY" />
-                            <Picker.Item label="ZAR / £" value="ZAR" />
+                            <Picker.Item label='EUR / €' value='EUR' />
+                            <Picker.Item label='USD / $' value='USD' />
+                            <Picker.Item label='AUD / $A' value='AUD' />
+                            <Picker.Item label='BGN / Lv' value='BGN' />
+                            <Picker.Item label='BRL / R$' value='BRL' />
+                            <Picker.Item label='CAD / C$' value='CAD' />
+                            <Picker.Item label='CHF / SFr' value='CHF' />
+                            <Picker.Item label='CNY / C$' value='CNY' />
+                            <Picker.Item label='CZK / C$' value='CZK' />
+                            <Picker.Item label='DKK / C$' value='DKK' />
+                            <Picker.Item label='GBP / £' value='GBP' />
+                            <Picker.Item label='HKD / £' value='HKD' />
+                            <Picker.Item label='HRK / £' value='HRK' />
+                            <Picker.Item label='HUF / £' value='HUF' />
+                            <Picker.Item label='IDR / £' value='IDR' />
+                            <Picker.Item label='ILS / £' value='ILS' />
+                            <Picker.Item label='INR / £' value='INR' />
+                            <Picker.Item label='JPY / £' value='JPY' />
+                            <Picker.Item label='KRW / £' value='KRW' />
+                            <Picker.Item label='MXN / £' value='MXN' />
+                            <Picker.Item label='MYR / £' value='MYR' />
+                            <Picker.Item label='NOK / £' value='NOKk' />
+                            <Picker.Item label='NZD / £' value='NZD' />
+                            <Picker.Item label='PHP / £' value='PHP' />
+                            <Picker.Item label='PLN / £' value='PLN' />
+                            <Picker.Item label='RON / £' value='RON' />
+                            <Picker.Item label='RUB / £' value='RUB' />
+                            <Picker.Item label='SEK / £' value='SEK' />
+                            <Picker.Item label='SGD / £' value='SGD' />
+                            <Picker.Item label='THB / £' value='THB' />
+                            <Picker.Item label='TRY / £' value='TRY' />
+                            <Picker.Item label='ZAR / £' value='ZAR' />
                         </Picker>
                     </View>
                 </KeyboardAvoidingView>
-                
+
                 <ScrollView>
                     {this.state.expense.photos.map((p: any, i: any) => {
                         return (
@@ -155,7 +150,7 @@ export class AddExpense extends Component<IDefaultNavProps, IState> {
                                 key={i}
                                 style={{
                                     width: 300,
-                                    height: 100,
+                                    height: 100
                                 }}
                                 source={{ uri: p.node.image.uri }}
                             />
@@ -163,10 +158,10 @@ export class AddExpense extends Component<IDefaultNavProps, IState> {
                     })}
                 </ScrollView>
 
-                <TouchableOpacity style={styles.buttonContainer} onPress={() =>  this._handleButtonPress}>
+                <TouchableOpacity style={styles.buttonContainer} onPress={() => this._handleButtonPress()}>
                     <Text style={styles.buttonText}>Add picture</Text>
                 </TouchableOpacity>
-                
+
                 <TouchableOpacity style={styles.buttonContainer} onPress={() => goBack()}>
                     <Text style={styles.buttonText}>BACK</Text>
                 </TouchableOpacity>
@@ -177,22 +172,21 @@ export class AddExpense extends Component<IDefaultNavProps, IState> {
             </View>
         );
     }
-    
 
     _handleButtonPress = () => {
         CameraRoll.getPhotos({
             first: 4,
-            assetType: 'All',
+            assetType: 'All'
         })
             .then(r => {
+                console.log(r.edges);
                 const expense = Object.assign({}, this.state.expense, { photos: r.edges });
                 this.setState({ expense });
             })
             .catch((err) => {
-                //Error Loading Images
+                console.log('error');
             });
     };
-
 
     save(goBack: any) {
         this.addExpenseToStorage()
@@ -225,7 +219,7 @@ export class AddExpense extends Component<IDefaultNavProps, IState> {
                 'currency': this.state.expense.currency,
                 'firstname': this.state.expense.firstname,
                 'lastname': this.state.expense.lastname,
-                'photos': this.state.expense.photos,
+                'photos': this.state.expense.photos
             });
 
             await AsyncStorage.setItem(this.state.expenseArrayId, JSON.stringify(this.state.expenseArray));
@@ -279,6 +273,6 @@ const styles = StyleSheet.create({
     image: {
         width: 100,
         height: 100,
-        margin: 10,
-    },
+        margin: 10
+    }
 });
