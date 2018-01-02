57d1ddcc23c425ed483673b8b3fb55f2d35d8372
ThomasCorthouts <thomas.corthouts@icloud.com>
2018-01-02 12:14:31 +0100

    improved expenseFeedItem

--- src/components/ExpenseFeedItem.tsx
+++ src/components/ExpenseFeedItem.tsx
@@ -13,20 +13,42 @@ export class ExpenseItem extends React.Component<IProps, {}> {
     }
 
     render() {
-        return (
-            <View key={this.props.keyval} >
-                <TouchableOpacity style={styles.item} onPress={this.props.viewDetails}>
-                    <View>
-                        <Text style={styles.detailText}>{this.props.val.balances[1].person.firstname} {this.props.val.balances[1].person.lastname}</Text>
-                        <Text style={styles.detailText}>⇨ {this.props.val.balances[0].person.firstname} {this.props.val.balances[0].person.lastname}</Text>
-                        <Text style={styles.detailTextSmall}>{this.props.val.description} - {this.props.val.category}</Text>
-                    </View>
-                    <View style={styles.expense}>
-                        <Text>{this.props.val.currency.symbol}{this.props.val.amount}</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-        );
+
+        if (this.props.val.splitOption && this.props.val.splitOption === 'Transaction') {
+            return (
+                <View key={this.props.keyval}>
+                    <TouchableOpacity style={styles.item} onPress={this.props.viewDetails}>
+                        <View>
+                            <Text
+                                style={styles.detailText}>{this.props.val.balances[1].person.firstname} {this.props.val.balances[1].person.lastname}</Text>
+                            <Text
+                                style={styles.detailText}>⇨ {this.props.val.balances[0].person.firstname} {this.props.val.balances[0].person.lastname}</Text>
+                            <Text
+                                style={styles.detailTextSmall}>{this.props.val.category} - {this.props.val.description}</Text>
+                        </View>
+                        <View style={styles.expense}>
+                            <Text>{this.props.val.currency.symbol}{this.props.val.amount}</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            );
+        } else {
+            return (
+                <View key={this.props.keyval}>
+                    <TouchableOpacity style={styles.item} onPress={this.props.viewDetails}>
+                        <View>
+                            <Text
+                                style={styles.detailText}>{this.props.val.description}</Text>
+                            <Text
+                                style={styles.detailTextSmall}>{this.props.val.category}</Text>
+                        </View>
+                        <View style={styles.expense}>
+                            <Text>{this.props.val.currency.symbol}{this.props.val.amount}</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            );
+        }
     }
 }
 
