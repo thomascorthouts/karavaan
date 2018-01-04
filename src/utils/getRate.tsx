d320d07c2e814e55b6f2bed9de190dcaa4c7081e
Mathias Spanhove <mathias.spanhove@student.ucll.be>
2018-01-04 23:21:59 +0100

    fixed currency conversion

--- src/utils/getRate.tsx
+++ src/utils/getRate.tsx
@@ -1,8 +1,5 @@
 export const getRate = (from: string, to: string, currencies: Currencies) => {
     const rateFrom = currencies[from].rate as number;
     const rateTo = currencies[to].rate as number;
-
-    console.log(currencies[to].base, to, rateTo, from, rateFrom);
-
     return rateTo / rateFrom;
 };
